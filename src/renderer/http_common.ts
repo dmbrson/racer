ab0c8c52e900bef51bc1b43132b6441429333eb7
danil <dka04@list.ru>
2024-05-19 23:47:59 +1000

    бэк таблицы рекордов и профиля

--- src/renderer/http_common.ts
+++ src/renderer/http_common.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "http://localhost:8000/api/",
   headers: {
     "Content-type": "application/json",
-    'Authorization': 'Bearer '+ localStorage.getItem('token')
-  }
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  },
 });
