33430995f6c15f639f6ff2787296fdbad6f8c1b0
IlushkaDV <magnum301076@gmail.com>
2024-05-23 01:53:48 +1000

    upgrade pause-menu

--- src/renderer/views/Pause.vue
+++ src/renderer/views/Pause.vue
@@ -1,43 +1,45 @@
 <template>
-    <div class="pause-menu">
-      <h1>Пауза</h1>
-      <button @click="resumeGame">Продолжить</button>
-      <button @click="restartGame">Перезапустить</button>
-      <button @click="exitToMenu">Выйти в меню</button>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { useRouter } from 'vue-router';
-  
-  const router = useRouter();
-  
-  const resumeGame = () => {
-    alert('Эта функция пока не доступна');
-  };
-  
-  const restartGame = () => {
-    router.push('/game');
-  };
-  
-  const exitToMenu = () => {
-    router.push('/');
-  };
-  </script>
-  
-  <style scoped>
-  .pause-menu {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: rgba(0, 0, 0, 0.7);
-    color: white;
-  }
-  
-  .pause-menu button {
-    display: inline-block;
+  <div class="pause-menu">
+    <h1>Пауза</h1>
+    <button @click="resumeGame">Продолжить</button>
+    <button @click="restartGame">Перезапустить</button>
+    <button @click="exitToMenu">Выйти в меню</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const resumeGame = () => {
+  alert('Эта функция пока не доступна');
+};
+
+const restartGame = () => {
+  router.push('/game');
+};
+
+const exitToMenu = () => {
+  router.push('/');
+};
+</script>
+
+<style lang="css" scoped>
+.pause-menu {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width : 30vw;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+ /* Добавленный отступ между кнопками и нижней частью меню */
+}
+
+.pause-menu button {
+  display: inline-block;
   padding: 10px 20px;
   font-size: 24px;
   font-weight: bold;
@@ -50,8 +52,10 @@
   box-shadow: 5px 5px 0px #000;
   transition: all 0.3s ease;
   cursor: pointer;
-  }
-  .pause-menu button:hover {
+  margin-bottom: 1em; /* Добавленный отступ между кнопками */
+}
+
+.pause-menu button:hover {
   background-color: #fff;
   color: #ff5252;
   border: 2px solid #ff5252;
@@ -63,5 +67,4 @@
   box-shadow: none;
   transform: translateY(4px);
 }
-  </style>
-  
+</style>
